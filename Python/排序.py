14c8425ca9366853873ac9286849c6b37c371f57
LCONG <1296867469@qq.com>
2022-06-08 22:42:00 +0800

    排序

--- Python/排序.py
+++ Python/排序.py
@@ -8,11 +8,22 @@ Created on Tue Jun  7 16:52:00 2022
 #排序
 li = [54,26,93,17,77,31,44,55,20]
 #1、冒泡排序
-def bouble_sort(alist):
-    for j in range(len(alist)-1,0,-1):  #j表示每次遍历需要比较的次数，逐渐减小
-        for i in range(j):
-            if alist[i] > alist[i+1]:   #如果前面的数比后面的数大，则交换位置
-                alist[i],alist[i+1] = alist[i+1],alist[i]
-bouble_sort(li)
-print(li[len(li)-1])    #打印最大的一位数
+# =============================================================================
+# def bouble_sort(alist):
+#     for j in range(len(alist)-1,0,-1):  #j表示每次遍历需要比较的次数，逐渐减小
+#         for i in range(j):
+#             if alist[i] > alist[i+1]:   #如果前面的数比后面的数大，则交换位置
+#                 alist[i],alist[i+1] = alist[i+1],alist[i]
+# bouble_sort(li)
+# print(li[len(li)-1 ])    #打印最大的一位数
+# =============================================================================
 
+#=============================================================================
+def bouble_s(lli):
+    for i in range(len(lli)):
+        for j in range(i):
+            if lli[j] > lli[j+1]:
+                lli[j],lli[j+1] = lli[j+1],lli[j]
+bouble_s(li)
+print(li)
+#=============================================================================
